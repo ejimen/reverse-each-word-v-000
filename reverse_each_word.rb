4c7d06e33b2835eb96c6fd0866b1710b3f24ebb6
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-30 19:43:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
-reverse_each_word = ["Hello there, and how are you?"]
+sentence1 = ["Hello there, and how are you?"]
 
-def reverse_each_word.reverse
-  reverse_each_word.each do |reverse_each_word|
+def reverse_each_word(sentence1)
+  sentence1.reverse
+  rsentence1.each do
     puts "#{reverse_each_word}"
   end
 end
