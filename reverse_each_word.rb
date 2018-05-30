52afdbd2bb11c6276ad8ada00552965ce415dfc1
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-30 19:19:58 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+sentence2 = ("Hello there, and how are you?")
+
+def reverse_each_word(sentence2)
+  sentence2.collect do |sentence|
+    puts "Hi again, just making sure it's reversed!"
+  end
+end
