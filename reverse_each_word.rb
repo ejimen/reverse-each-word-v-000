4016252705fb4d40cc85963ad6dcb5580586dac8
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-30 20:01:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,15 @@
-#sentence1 = ["Hello there, and how are you?"]
-
-#def reverse_each_word(sentence1)
-  #sentence1.reverse
-  #rsentence1.each do
-  #end
-#end
+def reverse_each_word(sentence)
+  sentence = sentence.split(" ")
+  sentence.each do
+    sentence.reverse
+  end
+end
 
 #reverse_each_word("Hello there, and how are you?")
 
-def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
-end
+#def reverse_each_word(sentence)
+  #words_array = sentence.split(" ")
+  #new_array = []
+  #new_array = words_array.collect {|word| word.reverse }
+  #new_array.join(" ")
+#end
